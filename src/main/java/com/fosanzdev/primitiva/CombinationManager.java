b195bf50057f1847a1037138b4c6743dee37350a
Esteban Sánchez <108540114+FosanzDev@users.noreply.github.com>
2023-02-22 11:18:32 +0100

    Added combination manager class
    CombinationManager randomly generates non-repeating numbers within the limits of the primitiva game

---
+++ src/main/java/com/fosanzdev/primitiva/CombinationManager.java
@@ -0,0 +1,72 @@
+package com.fosanzdev.test.primitiva;
+
+import java.util.Random;
+
+public class CombinationManager {
+
+    private static final Random random = new Random();
+
+    /**
+     * Generates a new random combination of 6 numbers between 0 and 49
+     * Generated numbers are not repeated
+     *
+     * @return a new random combination of 6 numbers between 0 and 49
+     */
+    public static int[] newCombination(){
+        //Create a new combination array
+        int[] combination = new int[6];
+
+        for (int i = 0; i < combination.length; i++) {
+            //Generate a new random number
+            int k = random.nextInt(49);
+
+            //Check if the number is already in the combination
+            while(contains(combination, k))
+                k = random.nextInt(49);
+
+            //Add the number to the combination
+            combination[i] = k;
+        }
+
+        //Return the combination
+        return combination;
+    }
+
+    /**
+     * Generates a new random number between 0 and 9
+     * @return a new random number between 0 and 9
+     */
+    public static int newReintegro(){
+        return random.nextInt(9);
+    }
+
+    /**
+     * Generates a new random number between 0 and 49
+     * @ return a new random number between 0 and 49
+     */
+    public static int newNumber(int[] ganadora){
+        int k = random.nextInt(49);
+        while(contains(ganadora, k)) {
+            k = random.nextInt(49);
+        }
+        return k;
+    }
+
+
+    /**
+     * Checks if a number is in a combination
+     * @param combination the combination to check
+     * @param k the number to check
+     * @return true if the number is in the combination, false otherwise
+     */
+    public static boolean contains(int[] combination, int k){
+        for (int j : combination) {
+            if (j == k) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
