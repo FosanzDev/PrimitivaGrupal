64e2028c04a4a257570ddeb1619ccee9ce32b640
adriaabf04 <adrianbf2004@gmail.com>
2023-02-22 12:54:51 +0100

    Creating the method checkWin
    This method do the checking of the player tickets and return the achievement which he wins

--- src/main/java/com/fosanzdev/primitiva/Category.java
+++ src/main/java/com/fosanzdev/primitiva/Category.java
@@ -1,5 +1,5 @@
 package com.fosanzdev.primitiva;
 
 public enum Category {
-    CAT_ESP,CAT_1,CAT_2,CAT_3,CAT_4,CAT_5
+    CAT_ESP,CAT_1,CAT_2,CAT_3,CAT_4,CAT_5,NONE
 }
--- src/main/java/com/fosanzdev/primitiva/Checking.java
+++ src/main/java/com/fosanzdev/primitiva/Checking.java
@@ -10,4 +10,42 @@ public class Checking {
         this.winReintegro = CombinationManager.newReintegro();
         this.winComplementalNum = CombinationManager.newNumber(winCombination);
     }
+
+    public Category checkWin(Boleto playerTicket) {
+        int contNumbers = 0;
+        boolean reintegro = false;
+        boolean complementalNum = false;
+        for (int i = 0; i < playerTicket.getNumeros().length; i++) {
+            if (CombinationManager.contains(winCombination,playerTicket.getNumeros()[i])) {
+                contNumbers++;
+            }
+            if(winComplementalNum == playerTicket.getNumeros()[i]) {
+                complementalNum = true;
+            }
+        }
+        if(winReintegro == playerTicket.getReintegro()) {
+            reintegro=true;
+        }
+
+        switch(contNumbers) {
+            case 1:
+            case 2:
+                return Category.NONE;
+            case 3:
+                return Category.CAT_5;
+            case 4:
+                return Category.CAT_4;
+            case 5:
+                if (complementalNum) {
+                    return Category.CAT_2;
+                }
+                return Category.CAT_3;
+            case 6:
+                if (reintegro){
+                    return Category.CAT_ESP;
+                }
+                return Category.CAT_1;
+        }
+        return Category.NONE;
+    }
 }
